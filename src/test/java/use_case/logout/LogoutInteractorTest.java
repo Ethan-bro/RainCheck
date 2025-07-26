facb8519d0dd99c7a1e3534ac7866723f03c5808
Ethan-bro <ethan.brook@mail.utoronto.ca>
2025-07-26 16:36:14 -0400

    LogoutInteractorTest.java -> Checking for successfull logout

---
+++ src/test/java/use_case/logout/LogoutInteractorTest.java
@@ -0,0 +1,37 @@
+package use_case.logout;
+
+import data_access.InMemoryUserDataAccessObject;
+import entity.CommonUserFactory;
+import entity.UserFactory;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class LogoutInteractorTest {
+
+    @Test
+    void successfulLogout() {
+        InMemoryUserDataAccessObject dao = new InMemoryUserDataAccessObject();
+        UserFactory factory = new CommonUserFactory();
+        dao.save(factory.create("Carol", "pass"));
+        dao.setCurrentUsername("Carol");
+
+        LogoutInputData input = new LogoutInputData("Carol");
+
+        LogoutOutputBoundary presenter = new LogoutOutputBoundary() {
+            @Override
+            public void prepareSuccessView(LogoutOutputData data) {
+                Assertions.assertEquals("Carol", data.getUsername());
+            }
+
+            @Override
+            public void prepareFailView(String error) {
+                Assertions.fail("Unexpected failure: " + error);
+            }
+        };
+
+        LogoutInputBoundary interactor = new LogoutInteractor(dao, presenter);
+        interactor.execute(input);
+
+        Assertions.assertNull(dao.getCurrentUsername());
+    }
+}
