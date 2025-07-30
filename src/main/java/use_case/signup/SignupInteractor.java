542d778923ca15c41c3f0cae4c7574af9203d90a
Ethan-bro <ethan.brook@mail.utoronto.ca>
2025-07-30 11:56:30 -0400

    Added more secure email checking

--- src/main/java/use_case/signup/SignupInteractor.java
+++ src/main/java/use_case/signup/SignupInteractor.java
@@ -3,6 +3,8 @@ package use_case.signup;
 import entity.User;
 import entity.UserFactory;
 
+import java.util.Set;
+
 /**
  * The Signup Interactor.
  */
@@ -10,6 +12,20 @@ public class SignupInteractor implements SignupInputBoundary {
     private final SignupUserDataAccessInterface userDataAccessObject;
     private final SignupOutputBoundary userPresenter;
     private final UserFactory userFactory;
+
+    // Allowed email domains list (can be expanded)
+    private static final Set<String> allowedDomains = Set.of(
+            "gmail.com", "yahoo.com", "outlook.com", "hotmail.com", "aol.com",
+            "icloud.com", "mail.com", "zoho.com", "protonmail.com", "live.com",
+            "msn.com", "comcast.net", "verizon.net", "att.net", "me.com",
+            "mail.ru", "yandex.ru", "qq.com", "gmx.com", "cox.net",
+            "hotmail.co.uk", "btinternet.com", "sbcglobal.net", "bellsouth.net", "rocketmail.com",
+            "aim.com", "yahoo.co.uk", "mac.com", "earthlink.net", "charter.net",
+            "shaw.ca", "telus.net", "sympatico.ca", "rogers.com", "mail.utoronto.ca",
+            "live.ca", "googlemail.com", "facebook.com", "verizon.com", "bt.com",
+            "inbox.com", "web.de", "naver.com", "hanmail.net", "qqmail.com",
+            "mailinator.com", "fastmail.com", "tutanota.com", "hushmail.com", "posteo.de"
+    );
 
     public SignupInteractor(SignupUserDataAccessInterface signupDataAccessInterface,
                             SignupOutputBoundary signupOutputBoundary,
@@ -28,26 +44,30 @@ public class SignupInteractor implements SignupInputBoundary {
         String repeatPassword = signupInputData.getRepeatPassword();
         String email = signupInputData.getEmail();
 
-        if (email.isEmpty()) {
+        if (email == null || email.isEmpty()) {
             errorMessages.append("Email cannot be empty.\n");
         } else if (!email.matches("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$")) {
             errorMessages.append("Invalid email format.\n");
+        } else {
+            // Check domain part after '@'
+            String domain = email.substring(email.indexOf("@") + 1).toLowerCase();
+            if (!allowedDomains.contains(domain)) {
+                errorMessages.append("Email domain not allowed.\n");
+            }
         }
 
-        if (username.isEmpty()) {
+        if (username == null || username.isEmpty()) {
             errorMessages.append("Username cannot be empty.\n");
         }
 
-        if (password.isEmpty()) {
+        if (password == null || password.isEmpty()) {
             errorMessages.append("Password cannot be empty.\n");
+        } else if (!password.equals(repeatPassword)) {
+            errorMessages.append("Passwords don't match.\n");
         }
 
         if (userDataAccessObject.existsByName(username)) {
             errorMessages.append("User already exists.\n");
-        }
-
-        if (!password.equals(repeatPassword)) {
-            errorMessages.append("Passwords don't match.\n");
         }
 
         if (!errorMessages.isEmpty()) {
