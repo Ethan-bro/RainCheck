1ebae8b2e2d6638865137b0190bedc093b7e2e34
Ethan-bro <ethan.brook@mail.utoronto.ca>
2025-08-02 15:02:33 -0400

    Changed the NO_TAG_SELECTED error

--- src/main/java/use_case/addTask/AddTaskError.java
+++ src/main/java/use_case/addTask/AddTaskError.java
@@ -7,7 +7,7 @@ public enum AddTaskError {
     EXCEEDS_CHAR_LIM("Task Name must be less than " +Constants.CHAR_LIMIT + " characters!"),
     EMPTY_TIME("Start Date/Time and End Date/Time cannot be empty!"),
     START_AFTER_END("End Date/Time must be after Start Date/Time"),
-    NO_TAG_SELECTED("You must create and select at least one custom tag!");
+    NO_TAG_SELECTED("Please create and select a tag first");
 
     private final String message;
 
