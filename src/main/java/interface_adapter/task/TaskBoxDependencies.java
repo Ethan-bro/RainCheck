6055177db0e826c1bb3a2d4b91a6696f3f376287
Ethan-bro <ethan.brook@mail.utoronto.ca>
2025-08-03 15:24:11 -0400

    Adding the EditTaskViewModel to the TaskBoxDep...

--- src/main/java/interface_adapter/task/TaskBoxDependencies.java
+++ src/main/java/interface_adapter/task/TaskBoxDependencies.java
@@ -3,11 +3,13 @@ package interface_adapter.task;
 import interface_adapter.ViewManagerModel;
 import interface_adapter.deleteTask.DeleteTaskViewModel;
 import interface_adapter.editTask.EditTaskController;
+import interface_adapter.editTask.EditTaskViewModel;
 import interface_adapter.markTaskComplete.MarkTaskCompleteViewModel;
 
 public record TaskBoxDependencies(
         MarkTaskCompleteViewModel markTaskCompleteViewModel,
         DeleteTaskViewModel deleteTaskViewModel,
         ViewManagerModel viewManagerModel,
-        EditTaskController editTaskController
+        EditTaskController editTaskController,
+        EditTaskViewModel editTaskViewModel
 ) {}
