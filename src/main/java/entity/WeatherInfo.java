4c6c1b8af55b1f0827ee8dd469a9784950221565
Ethan-bro <ethan.brook@mail.utoronto.ca>
2025-08-03 17:14:26 -0400

    Refactored code to use new entity WeatherInfo and use_case weather/WeatherInfoGetter

--- src/main/java/entity/WeatherInfo.java
+++ src/main/java/entity/WeatherInfo.java
@@ -1,20 +1,4 @@
 package entity;
 
-public class WeatherInfo {
-    private final String description;
-    private final String feels;
-    private final String iconName;
-    private final String temperature;
-
-    public WeatherInfo(String description, String feels, String iconName, String temperature) {
-        this.description = description;
-        this.feels = feels;
-        this.iconName = iconName;
-        this.temperature = temperature;
-    }
-
-    public String getDescription() { return description; }
-    public String getFeels() { return feels; }
-    public String getIconName() { return iconName; }
-    public String getTemperature() { return temperature; }
+public record WeatherInfo(String description, String iconName, String temperature) {
 }
