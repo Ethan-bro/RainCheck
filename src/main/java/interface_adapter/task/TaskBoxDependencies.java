3e3f7f305d529b5cfb3a8b429120079bc7fe0456
Ethan-bro <ethan.brook@mail.utoronto.ca>
2025-08-03 14:38:15 -0400

    Turning TaskBoxDep... into a record class

--- src/main/java/interface_adapter/task/TaskBoxDependencies.java
+++ src/main/java/interface_adapter/task/TaskBoxDependencies.java
@@ -6,38 +6,8 @@ import interface_adapter.markTaskComplete.MarkTaskCompleteViewModel;
 import interface_adapter.deleteTask.DeleteTaskController;
 import interface_adapter.editTask.EditTaskController;
 
-public class TaskBoxDependencies {
-    public final TaskViewModel taskViewModel;
-    public final MarkTaskCompleteViewModel markTaskCompleteViewModel;
-    public final ViewManagerModel viewManagerModel;
-    public final MarkTaskCompleteController markTaskCompleteController;
-    public final DeleteTaskController deleteTaskController;
-    public final EditTaskController editTaskController;
-    private String taskDependenciesUsername;
-
-    public TaskBoxDependencies(TaskViewModel taskViewModel,
-                               MarkTaskCompleteViewModel markTaskCompleteViewModel,
-                               ViewManagerModel viewManagerModel,
-                               MarkTaskCompleteController markTaskCompleteController,
-                               DeleteTaskController deleteTaskController,
-                               EditTaskController editTaskController) {
-        this.taskViewModel = taskViewModel;
-        this.markTaskCompleteViewModel = markTaskCompleteViewModel;
-        this.viewManagerModel = viewManagerModel;
-        this.markTaskCompleteController = markTaskCompleteController;
-        this.deleteTaskController = deleteTaskController;
-        this.editTaskController = editTaskController;
-    }
-
-    public TaskViewModel getTaskViewModel() {
-        return taskViewModel;
-    }
-
-    public void setTaskDependenciesUsername(String taskDependenciesUsername) {
-        this.taskDependenciesUsername = taskDependenciesUsername;
-    }
-
-    public String getTaskDependenciesUsername() {
-        return taskDependenciesUsername;
-    }
+public record TaskBoxDependencies(TaskViewModel taskViewModel, MarkTaskCompleteViewModel markTaskCompleteViewModel,
+                                  ViewManagerModel viewManagerModel,
+                                  MarkTaskCompleteController markTaskCompleteController,
+                                  DeleteTaskController deleteTaskController, EditTaskController editTaskController) {
 }
