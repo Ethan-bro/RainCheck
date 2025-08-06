334e7006f3ee474b3b543288dcf124a78707507f
Wang-Clara <clarag.wang@mail.utoronto.ca>
2025-08-06 17:34:00 -0400

    mostly completed edit task interactor tests

--- src/test/java/use_case/editTask/EditTaskInteractorTest.java
+++ src/test/java/use_case/editTask/EditTaskInteractorTest.java
@@ -3,7 +3,7 @@ package use_case.editTask;
 import data_access.InMemoryTaskDataAccessObject;
 import data_access.WeatherApiService;
 import entity.*;
-import org.jetbrains.annotations.NotNull;
+import interface_adapter.addTask.Constants;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -103,6 +103,163 @@ public class EditTaskInteractorTest {
                 Assertions.assertEquals("Task not found.", errorMessage);
             }
         };
+        EditTaskInteractor interactor = new EditTaskInteractor(dao, presenter, new WeatherApiService());
+        EditTaskInputData input = new EditTaskInputData(username, updatedTask);
+        interactor.execute(username, input);
+    }
+
+    @Test
+    void failEditEmptyTaskName() throws IOException {
+        InMemoryTaskDataAccessObject dao = new InMemoryTaskDataAccessObject();
+
+        TaskID taskId = new TaskID(UUID.randomUUID());
+
+        TaskInfo originalInfo = new TaskInfo(
+                taskId, "Original Task",
+                LocalDateTime.now(),
+                LocalDateTime.now().plusHours(1),
+                Priority.LOW,
+                new CustomTag("Tag", "👽"),
+                null,
+                "No",
+                "",
+                "",
+                "25.0",
+                "2");
+        Task originalTask = new Task(originalInfo);
+        dao.addTask(username, originalTask);
+
+        TaskInfo updatedInfo = new TaskInfo(
+                taskId, "",
+                LocalDateTime.now(),
+                LocalDateTime.now().plusHours(1),
+                Priority.LOW,
+                new CustomTag("Tag", "👽"),
+                null,
+                "No",
+                "",
+                "",
+                "25.0",
+                "2");
+        Task updatedTask = new Task(updatedInfo);
+
+        EditTaskOutputBoundary presenter = new EditTaskOutputBoundary() {
+            @Override
+            public void prepareSuccessView(EditTaskOutputData outputData) {
+                Assertions.fail("Expected task update to fail, but it succeeded");
+            }
+
+            @Override
+            public void prepareFailView(String errorMessage) {
+                Assertions.assertEquals("Task name cannot be empty.", errorMessage);
+            }
+        };
+
+        EditTaskInteractor interactor = new EditTaskInteractor(dao, presenter, new WeatherApiService());
+        EditTaskInputData input = new EditTaskInputData(username, updatedTask);
+        interactor.execute(username, input);
+    }
+
+    @Test
+    void failEditTaskNameTooLong() throws IOException {
+        InMemoryTaskDataAccessObject dao = new InMemoryTaskDataAccessObject();
+
+        TaskID taskId = new TaskID(UUID.randomUUID());
+
+        TaskInfo originalInfo = new TaskInfo(
+                taskId, "Original Task",
+                LocalDateTime.now(),
+                LocalDateTime.now().plusHours(1),
+                Priority.HIGH,
+                new CustomTag("Tag", "🌷"),
+                null,
+                "No",
+                "",
+                "",
+                "25.0",
+                "2");
+        Task originalTask = new Task(originalInfo);
+        dao.addTask(username, originalTask);
+
+        TaskInfo updatedInfo = new TaskInfo(
+                taskId, "UpdatedUpdatedUpdatedUpdatedUpdatedUpdatedUpdatedUpdatedUpdatedUpdated",
+                LocalDateTime.now(),
+                LocalDateTime.now().plusHours(1),
+                Priority.HIGH,
+                new CustomTag("Tag", "🌷"),
+                null,
+                "No",
+                "",
+                "",
+                "25.0",
+                "2");
+        Task updatedTask = new Task(updatedInfo);
+
+        EditTaskOutputBoundary presenter = new EditTaskOutputBoundary() {
+            @Override
+            public void prepareSuccessView(EditTaskOutputData outputData) {
+                Assertions.fail("Expected task update to fail, but it succeeded");
+            }
+
+            @Override
+            public void prepareFailView(String errorMessage) {
+                Assertions.assertTrue(
+                        errorMessage.contains("Task name exceeds character limit of " + Constants.CHAR_LIMIT)
+                );
+            }
+        };
+
+        EditTaskInteractor interactor = new EditTaskInteractor(dao, presenter, new WeatherApiService());
+        EditTaskInputData input = new EditTaskInputData(username, updatedTask);
+        interactor.execute(username, input);
+    }
+
+    @Test
+    void failEditEndTimeBeforeStart() throws IOException {
+        InMemoryTaskDataAccessObject dao = new InMemoryTaskDataAccessObject();
+        TaskID taskId = new TaskID(UUID.randomUUID());
+
+        TaskInfo originalInfo = new TaskInfo(
+                taskId, "Original Task",
+                LocalDateTime.now(),
+                LocalDateTime.now().plusHours(1),
+                Priority.HIGH,
+                new CustomTag("Tag", "🌷"),
+                null,
+                "No",
+                "",
+                "",
+                "25.0",
+                "2");
+        Task originalTask = new Task(originalInfo);
+        dao.addTask(username, originalTask);
+
+        TaskInfo updatedInfo = new TaskInfo(
+                taskId, "Updated Task",
+                LocalDateTime.now().plusHours(1),
+                LocalDateTime.now(),
+                Priority.HIGH,
+                new CustomTag("Tag", "🌷"),
+                null,
+                "No",
+                "",
+                "",
+                "25.0",
+                "2");
+        Task updatedTask = new Task(updatedInfo);
+
+        EditTaskOutputBoundary presenter = new EditTaskOutputBoundary() {
+            @Override
+            public void prepareSuccessView(EditTaskOutputData outputData) {
+                Assertions.fail("Expected task update to fail, but it succeeded");
+            }
+
+            @Override
+            public void prepareFailView(String errorMessage) {
+                Assertions.assertEquals("End time must be after start time.", errorMessage);
+            }
+        };
+
         EditTaskInteractor interactor = new EditTaskInteractor(dao, presenter, new WeatherApiService());
         EditTaskInputData input = new EditTaskInputData(username, updatedTask);
         interactor.execute(username, input);
